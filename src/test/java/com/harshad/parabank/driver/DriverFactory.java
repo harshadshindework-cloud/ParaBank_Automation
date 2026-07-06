24474389b561c1164fde39109ed77b7f8ed60847
Harshadshinde7620 <harshad.shinde@xenneotech.com>
2026-07-06 13:30:15 +0530

    feat: implement DriverFactory

---
+++ src/test/java/com/harshad/parabank/driver/DriverFactory.java
@@ -0,0 +1,43 @@
+package com.harshad.parabank.driver;
+
+import com.harshad.parabank.config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver() {
+
+        String browser = ConfigReader.getProperty("browser");
+
+        WebDriver driver;
+
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+
+            default:
+                throw new RuntimeException("Unsupported browser: " + browser);
+        }
+
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
--- src/test/java/com/harshad/parabank/ui/SmokeTest.java
+++ src/test/java/com/harshad/parabank/ui/SmokeTest.java
@@ -2,6 +2,7 @@ package com.harshad.parabank.ui;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.harshad.parabank.config.ConfigReader;
 
 public class SmokeTest {
 
@@ -13,4 +14,17 @@ public class SmokeTest {
         Assert.assertTrue(true);
 
     }
+
+    @Test
+    public void verifyConfigReader() {
+    String browser = ConfigReader.getProperty("browser");
+    String baseUrl = ConfigReader.getProperty("base.url");
+
+    System.out.println("Browser : " + browser);
+    System.out.println("Base URL : " + baseUrl);
+
+    Assert.assertEquals(browser, "chrome");
+    Assert.assertTrue(baseUrl.contains("parabank"));
+}
+
 }
